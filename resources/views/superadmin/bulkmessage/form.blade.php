26312157f1e0932582e865d8e8ae1aad4e39a55f
artic0909 <dev.saklin@gmail.com>
2026-07-27 17:51:23 +0530

    dasd

--- resources/views/superadmin/bulkmessage/form.blade.php
+++ resources/views/superadmin/bulkmessage/form.blade.php
@@ -5,7 +5,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Super | About Page</title>
+    <title>Day One - Five Bulk Message</title>
 
     <!-- plugins:css -->
     <link rel="stylesheet" href="{{asset('../partner-assets')}}">
